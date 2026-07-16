5691896db0459e37606710782f259d5aeb71326b
kcomito <katherinecomito@gmail.com>
2026-07-15 22:01:48 -0400

    Initalize CampusFlow React app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import "./App.css";
+import "./App.css"; // Stylesheet connected
 
 function App() {
-  return (
-    <main className="landing-page">
-      <section className="hero">
-        <p className="eyebrow">Campus event planning made easier</p>
+  return ( // return statment, tells React what should appear on the page
+    <main className="landing-page"> {/* Outermost JSX element */}
+      <section className="hero"> {/* Child JSX element*/}
+        <p className="eyebrow">Campus event planning made easier</p> {/* JSX element*/}
 
-        <h1>CampusFlow</h1>
+        <h1>CampusFlow</h1> {/**JSX (Javascript XML) write code similar to HTML inside JS */}
 
         <p className="hero-description">
           Plan events, organize tasks, and build stronger campus communities.
         </p>
 
-        <button type="button">Get Started</button>
+         {/* type=button is a regular button not form-submission button*/}
+        <button type="button">Get Started</button> {/**Everything between paratheses is JSX */}
       </section>
     </main>
   );
 }
 
-export default App;
+export default App; // makes App component available for another file to export
+// Notes:
