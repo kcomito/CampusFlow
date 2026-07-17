bb7b9db517523add09e59ccad60a78ff6408045b
kcomito <katherinecomito@gmail.com>
2026-07-17 15:58:08 -0400

    Add CampusFlow page routing and navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,17 @@ import { NavLink } from "react-router";
 function NavBar(){
     return (
         <nav>
-            {/*  */}
+            {/* className={({ isActive }) => {
+                if (isActive) {
+                    return "active-link";
+                }
+
+                return "";
+                }} */}
             <NavLink to="/" className={({ isActive }) => (isActive ? "active-link" : "")}>Home</NavLink>
-            <NavLink to="/login">Login</NavLink>
-            <NavLink to="/dashboard">Dashboard</NavLink>
+
+            <NavLink to="/login" className={({ isActive}) => (isActive ? "active-link" : "")}>Login</NavLink>
+            <NavLink to="/dashboard" className={({ isActive}) => (isActive ? "active-link" : "")}>Dashboard</NavLink>
         </nav>
 
     );
